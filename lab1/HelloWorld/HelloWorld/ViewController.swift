76682fb98e724250c621a637a3e297077da07be5
Yunyan Shi <YS@Yunyans-MacBook.local>
2019-12-30 07:27:00 -0800

    Add lab1

---
+++ lab1/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,21 @@
+//  Name: Yunyan Shi
+
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Neil Smyth on 10/6/18.
+//  Copyright © 2018 eBookFrenzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
